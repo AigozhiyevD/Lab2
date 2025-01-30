5068a8dbba68870fde7a852e2369f879ba307e33
AigozhiyevD <dim.taigo@gmail.com>
2025-01-31 02:53:40 +0500

    Added file

---
+++ PyhtonOperators.py
@@ -0,0 +1,21 @@
+print(10 + 5)
+
+print((6 + 3) - (6 + 3))
+
+x = 5
+y = 10
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+m = 5
+n = 3
+print(m & n)
+print(m | n)
+print(m ^ n)
+print(~m)
+print(m << 1)
+print(m >> 1)
