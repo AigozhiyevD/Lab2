6b67508fe2914b973d3d6ddf97e5f02d1889792d
AigozhiyevD <dim.taigo@gmail.com>
2025-01-31 03:07:36 +0500

    Added Folder

---
+++ PythonTuples/AccessTuple.py
@@ -0,0 +1,9 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+print(thistuple[-1])
+print(thistuple[1:2])
+print(thistuple[:2])
+print(thistuple[2:])
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
---
+++ PythonTuples/TupleMethods.py
@@ -0,0 +1,8 @@
+numbers = (1, 2, 3, 1, 4, 1, 5)
+print(numbers.count(1))
+print(numbers.count(3))
+print(numbers.count(6))
+
+fruits = ('apple', 'banana', 'cherry', 'banana', 'orange')
+print(fruits.index('banana'))
+print(fruits.index('cherry'))
